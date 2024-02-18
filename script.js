22ce783650a1ac2cacc13c4417a8b500453e4f26
kom483 <preetkoaml1130@gmail.com>
2024-02-18 18:25:12 +0530

    add practice ques

--- script.js
+++ script.js
@@ -9,10 +9,39 @@
 
 //ternary operators
 //let age = 25;
- //age>20 ? console.log("you can vote") : console.log("you can't vote");
+//age>20 ? console.log("you can vote") : console.log("you can't vote");
 
- //alert("hello world!!") this is only a alert message..
+//alert("hello world!!") this is only a alert message..
 //prompt('hello') prompt firstly print hello then ask for input..
 
-let name = prompt ("hello");
- console.log(name);
+//let name = prompt ("hello");
+//console.log(name);
+
+
+//get user to input a number using prompt("enter a number:"). check if the number is a multiple of 5 or not.
+let num = prompt("enter a number:");
+
+if(num % 3 === 0){
+    console.log(num, "is multiple of 3");
+}
+else{
+    console.log(num, "is not a multiple of 3");
+}
+
+
+//practiec q2
+let score = 66;
+let grade;
+if(score >= 90 && score <= 100){
+    grade ="A";
+}
+else if(score >=70 && score <=89){
+    grade = "B";
+}
+else if(score >= 60 && score <=69){
+    grade = "C";
+}
+else if(score >= 50 && score <=49){
+    grade = "F";
+}
+console.log("according to grade, your grade was :", grade);
