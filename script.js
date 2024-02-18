3aba7ef1c444c48890c96f8a58ec6297540df276
kom483 <preetkoaml1130@gmail.com>
2024-02-18 16:10:56 +0530

    add ternary opeartos

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
-let age = 15;
-if(age>= 18){
-    console.log("You can vote");
-}
-else{
-    console.log("you cannot vote");
-}
+// let age = 15;
+// if(age>= 18){
+//     console.log("You can vote");
+// }
+// else{
+//     console.log("you cannot vote");
+// }
+
+
+//ternary operators
+let age = 25;
+ let result = age>20 ? "you can vote" : "you can not vote";
+ console.log(result);
